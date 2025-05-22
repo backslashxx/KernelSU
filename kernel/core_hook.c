ddf4e9cf7103b38fd398d32b30cebfafed812b43
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-05-22 23:32:58 +0800

    kernel: core_hook: screw path_umount backport, call sys_umount directly
    this also fixups a UAF on ksu_umount_mnt
    
    turns out path_umount backport is completely unneeded
    old: https://github.com/tiann/KernelSU/commit/f541e0d7da745ae36fc4ee46c18192fc25d0ccbe
    
    EDIT:
    - add a fix for 4.17~5.8
    
    `fs: add ksys_umount() helper; remove in-kernel call to sys_umount()` https://github.com/torvalds/linux/commit/3a18ef5c1b3935cb05888fc37964321f7bd6231d
    - which adds a ksys_umount helper, basically turning sys_umount
    
    `syscalls/core: Introduce CONFIG_ARCH_HAS_SYSCALL_WRAPPER=y` https://github.com/torvalds/linux/commit/1bd21c6c21e848996339508d3ffb106d505256a8
    - which undefines in-kernel calls of syscalls, which is enabled on 4.19's arch/arm64/Kconfig
    
    Ref:
    - https://github.com/torvalds/linux/commits/v4.17/include/linux/syscalls.h
    - https://github.com/rsuntk/KernelSU/commit/d20f15e
    
    Reported-by: rsuntk <90097027+rsuntk@users.noreply.github.com>
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -62,12 +62,15 @@ int ksu_handle_rename(struct dentry *old_dentry, struct dentry *new_dentry)
 
 extern int path_umount(struct path *path, int flags);
 
-static void ksu_umount_mnt(struct path *path, int flags)
+static void ksu_umount_mnt(const char *mnt, struct path *path, int flags)
 {
 	int err = path_umount(path, flags);
-	if (err) {
-		pr_info("umount %s failed: %d\n", path->dentry->d_iname, err);
-	}
+
+	// upstream actually has a UAF here: path->dentry after dput
+	// but its fine as umount always succeeds
+	// that code path is very cold
+	if (err)
+		pr_info("umount %s failed: %d\n", mnt, err);
 }
 
 static void try_umount(const char *mnt, int flags)
@@ -84,7 +87,7 @@ static void try_umount(const char *mnt, int flags)
 		return;
 	}
 
-	ksu_umount_mnt(&path, flags);
+	ksu_umount_mnt(mnt, &path, flags);
 }
 
 int ksu_handle_setuid(struct cred *new, const struct cred *old)
