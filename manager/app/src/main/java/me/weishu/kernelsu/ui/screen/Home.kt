48d70802bbd83e7dc2caf4624316fc7fd8186815
rsuntk <90097027+rsuntk@users.noreply.github.com>
2025-08-29 16:12:52 +0800

    manager: Add ABI and Kernel archirecture info into InfoCardItem
    manager: simplify kernel arch
    
    Previously:
    
    Kernel
    4.19.331-Rissu
    
    Kernel Arch
    armv8l
    
    This changes:
    
    Kernel
    4.19.331-Rissu (armv8l)
    
    Suggested-by: backslashxx <118538522+backslashxx@users.noreply.github.com>
    Signed-off-by: rsuntk <90097027+rsuntk@users.noreply.github.com>

--- manager/app/src/main/java/me/weishu/kernelsu/ui/screen/Home.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/Home.kt
@@ -656,7 +656,7 @@ private fun InfoCard() {
         ) {
             InfoText(
                 title = stringResource(R.string.home_kernel),
-                content = uname.release
+                content = "${uname.release} (${uname.machine})"
             )
             InfoText(
                 title = stringResource(R.string.home_manager_version),
@@ -665,6 +665,10 @@ private fun InfoCard() {
             InfoText(
                 title = stringResource(R.string.home_fingerprint),
                 content = Build.FINGERPRINT
+            )
+            InfoText(
+                title = stringResource(R.string.home_abi),
+                content = Build.SUPPORTED_ABIS.joinToString(", ")
             )
             InfoText(
                 title = stringResource(R.string.home_selinux_status),
