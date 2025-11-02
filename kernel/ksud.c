5f230a0feafb0e35bc7eabdf1b39831e00d005a3
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-11-02 10:01:02 +0800

    Revert "kernel: transition devpts in kernel"
    Reverts 98757bcdb35ba20a1c11399173ceeab571d64b50
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/ksud.c
+++ kernel/ksud.c
@@ -58,8 +58,6 @@ bool ksu_execveat_hook __read_mostly = true;
 bool ksu_input_hook __read_mostly = true;
 #endif
 
-u32 ksu_devpts_sid;
-
 void on_post_fs_data(void)
 {
     static bool done = false;
@@ -73,9 +71,6 @@ void on_post_fs_data(void)
     ksu_observer_init();
     // sanity check, this may influence the performance
     stop_input_hook();
-
-    ksu_devpts_sid = ksu_get_devpts_sid();
-    pr_info("devpts sid: %d\n", ksu_devpts_sid);
 }
 
 #define MAX_ARG_STRINGS 0x7FFFFFFF
