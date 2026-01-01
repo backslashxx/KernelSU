4d83b8194a7ff2cf79345bcbf497158b97af4830
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-01-01 09:42:02 +0800

    kernel: ksud/lsm_hook: migrate ksu_install_rc_hook to security_file_permission LSM
    This removes the need for manually hooking sys_read / vfs_read.
    
    However we only do this if syscall table hook is disabled. that will be added in the next few commits after this
    
    devlog
    https://github.com/backslashxx/KernelSU/compare/5ba658b617459b035160def783fffaf6ea127c17...8a6ae2518f190374b76fd302d19ce6a4076a2928
    https://github.com/backslashxx/KernelSU/compare/b7df5d197fea934bf7e8a4537644fe5a2705f61e...754bbd59185f0c6cede2724b46d7be7c554dafc9
    
    EDIT: add unhook routine
    devlog: https://github.com/backslashxx/KernelSU/commits/444264ff7cc651c5ad398a98d02da6da12dae985
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/hook/lsm_hooks_list.c
+++ kernel/hook/lsm_hooks_list.c
@@ -36,6 +36,15 @@ static __nocfi void ksu_bprm_committing_creds(struct linux_binprm *bprm)
 	bprm_committing_creds_fn(bprm); // NOTE: void LSM hook
 }
 
+static int (*file_permission_fn)(struct file *file, int mask) __read_mostly = nullptr;
+static __nocfi int ksu_file_permission(struct file *file, int mask)
+{
+	if (unlikely(ksu_vfs_read_hook))
+		ksu_install_rc_hook(file);
+
+	return file_permission_fn(file, mask);
+}
+
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0) || defined(KSU_COMPAT_SECURITY_DELETE_HOOKS_HLIST)
 static void ksu_hack_lsm_slot(struct hlist_head *hook_head, uintptr_t *old_ptr, uintptr_t new_ptr)
 {
@@ -115,12 +124,32 @@ do {														\
 	ksu_hack_lsm_slot(&security_hook_heads.hook_name, &dummy_int, (uintptr_t)hook_name##_fn);		\
 } while (0)
 
+static int ksu_restore_file_permission(void *data)
+{
+	set_user_nice(current, 19); // low prio
+
+loop_start:
+	msleep(1000);
+	if (*(volatile bool *)&ksu_vfs_read_hook)
+		goto loop_start;
+
+	msleep(1000);
+
+	LSM_HACK_RESTORE(file_permission);
+	return 0;
+}
 
 static __init void ksu_lsm_hook_init(void)
 {
 	LSM_HACK_INIT(task_fix_setuid, ksu_task_fix_setuid);
 	LSM_HACK_INIT(inode_rename, ksu_inode_rename);
 	LSM_HACK_INIT(bprm_committing_creds, ksu_bprm_committing_creds);
+
+#if !defined(CONFIG_KSU_TAMPER_SYSCALL_TABLE) && !defined(CONFIG_KSU_HACK_ARM64_BRANCH_LINK)
+	LSM_HACK_INIT(file_permission, ksu_file_permission);
+	kthread_run(ksu_restore_file_permission, NULL, "kthread");
+#endif
+
 }
 
 static void __init ksu_core_init(void)
