712bdca720397eba667e39a118b2b8d4d8513799
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-01-01 09:42:02 +0800

    kernel: ksud: migrate ksu_install_rc_hook to security_file_permission LSM
    This removes the need for manually hooking sys_read / vfs_read.
    
    old: https://github.com/backslashxx/KernelSU/commit/246f8db975ed87df3298ac919f5ef905a9e51de0
    
    devlog
    https://github.com/backslashxx/KernelSU/compare/5ba658b617459b035160def783fffaf6ea127c17...8a6ae2518f190374b76fd302d19ce6a4076a2928
    https://github.com/backslashxx/KernelSU/compare/b7df5d197fea934bf7e8a4537644fe5a2705f61e...754bbd59185f0c6cede2724b46d7be7c554dafc9
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/hook/core_hook.c
+++ kernel/hook/core_hook.c
@@ -19,7 +19,50 @@ int ksu_bprm_check(struct linux_binprm *bprm)
 	return 0;
 }
 
+int ksu_file_permission(struct file *file, int mask)
+{
+	if (unlikely(ksu_vfs_read_hook))
+		ksu_install_rc_hook(file);
+
+	return 0;
+}
+
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 2, 0)
+
+static struct security_hook_list ksu_hooks_temp[] = {
+	LSM_HOOK_INIT(file_permission, ksu_file_permission),
+};
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0) || defined(KSU_COMPAT_SECURITY_DELETE_HOOKS_HLIST)
+#define delete_lsm_entry hlist_del_rcu
+#else
+#define delete_lsm_entry list_del_rcu
+#endif
+
+// see security_delete_hooks
+static inline void ksu_security_delete_hooks(struct security_hook_list *hooks, int count)
+{
+	int i;
+	for (i = 0; i < count; i++)
+		delete_lsm_entry(&hooks[i].list);
+}
+
+static int ksu_lsm_hook_restore(void *data)
+{
+
+loop_start:
+
+	msleep(1000);
+
+	if (*(volatile bool *)&ksu_vfs_read_hook)
+		goto loop_start;
+
+	pr_info("%s: unreg file_permission LSM!\n", __func__);
+
+	ksu_security_delete_hooks(ksu_hooks_temp, ARRAY_SIZE(ksu_hooks_temp));
+	return 0;
+}
+
 static struct security_hook_list ksu_hooks[] = {
 	LSM_HOOK_INIT(inode_rename, ksu_inode_rename),
 	LSM_HOOK_INIT(task_fix_setuid, ksu_task_fix_setuid),
@@ -30,11 +73,17 @@ static struct security_hook_list ksu_hooks[] = {
 static void ksu_lsm_hook_init(void)
 {
 	security_add_hooks(ksu_hooks, ARRAY_SIZE(ksu_hooks), "ksu");
+	security_add_hooks(ksu_hooks_temp, ARRAY_SIZE(ksu_hooks_temp), "ksu");
+
+	kthread_run(ksu_lsm_hook_restore, NULL, "unhook");
 }
 #else
 static void ksu_lsm_hook_init(void)
 {
 	security_add_hooks(ksu_hooks, ARRAY_SIZE(ksu_hooks));
+	security_add_hooks(ksu_hooks_temp, ARRAY_SIZE(ksu_hooks_temp));
+
+	kthread_run(ksu_lsm_hook_restore, NULL, "unhook");
 }
 #endif
 #endif // 4.2 || KSU_COMPAT_SECURITY_ADD_HOOKS_V2
