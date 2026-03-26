29d66d66b7a9c72716d2888a4d43f3c8064cb7a1
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-03-26 18:47:04 +0800

    kernel: prepare for legacy support
    Since upstream has way different methods, via kprobes/kretprobes/LKM/tracepoints,
    cleaning up support for unsupportable code is kind of a must. This simplifies porting
    downstream changes, restoring old components, debloat, and makes it easier to maintain
    downstream, e.g. avoiding excessive use of conditionals (ifdef hell).
    
    Theres now massive code divergence so this has to be done.
    We will restore compatibility to upstream with the next commits.
    
    This also restores compatibility code needed for legacy that was removed upstream.
    Restores an ifdef on arch.h, adds in-32-bit arm support.
    module_init -> device_initcall, lifetime is forever, so we dont need exit
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/selinux/selinux.h
+++ kernel/selinux/selinux.h
@@ -1,11 +1,12 @@
 #ifndef __KSU_H_SELINUX
 #define __KSU_H_SELINUX
 
-#include "linux/types.h"
-#include "linux/version.h"
-#include "linux/cred.h"
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)) || defined(KSU_COMPAT_HAS_SELINUX_STATE)
+#define KSU_COMPAT_USE_SELINUX_STATE
+#endif
 
-#define KERNEL_SU_DOMAIN "ksu"
+// TODO: rename to "ksu"
+#define KERNEL_SU_DOMAIN "su"
 #define KERNEL_SU_FILE "ksu_file"
 
 #define KERNEL_SU_CONTEXT "u:r:" KERNEL_SU_DOMAIN ":s0"
@@ -21,20 +22,18 @@ bool getenforce();
 
 void cache_sid(void);
 
-bool is_task_ksu_domain(const struct cred *cred);
+bool is_task_ksu_domain(const struct cred* cred);
 
 bool is_ksu_domain();
 
-bool is_zygote(const struct cred *cred);
+bool is_zygote(const struct cred* cred);
 
-bool is_init(const struct cred *cred);
+bool is_init(const struct cred* cred);
 
 void apply_kernelsu_rules();
 
 int handle_sepolicy(void __user *user_data, u64 data_len);
 
 void setup_ksu_cred();
-
-extern u32 ksu_file_sid;
 
 #endif
