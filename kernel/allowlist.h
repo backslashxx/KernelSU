3143e0370de88a1041300458fa840dfab2a6c5d4
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-03-27 13:38:33 +0800

    we need to see the compiler as an enemy

--- kernel/allowlist.h
+++ kernel/allowlist.h
@@ -1,8 +1,6 @@
 #ifndef __KSU_H_ALLOWLIST
 #define __KSU_H_ALLOWLIST
 
-#include "app_profile.h"
-
 #define PER_USER_RANGE 100000
 #define FIRST_APPLICATION_UID 10000
 #define LAST_APPLICATION_UID 19999
@@ -30,8 +28,8 @@ bool ksu_get_allow_list(int *array, u16 length, u16 *out_length, u16 *out_total,
 void ksu_prune_allowlist(bool (*is_uid_exist)(uid_t, char *, void *), void *data);
 void ksu_persistent_allow_list();
 
-bool ksu_get_app_profile(struct app_profile *);
-int ksu_set_app_profile(struct app_profile *);
+bool ksu_get_app_profile(struct app_profile *profile);
+int ksu_set_app_profile(struct app_profile *profile);
 
 bool ksu_uid_should_umount(uid_t uid);
 void ksu_get_root_profile(uid_t uid, struct root_profile *);
