cc9f81e60dcefdc8fac9cdb0e1ed468c9e5d247c
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-03-27 13:10:44 +0800

    app_profile

--- kernel/include/uapi/app_profile.h
+++ kernel/include/uapi/app_profile.h
@@ -1,32 +1,32 @@
-#ifndef __KSU_H_APP_PROFILE
-#define __KSU_H_APP_PROFILE
+#ifndef __KSU_UAPI_APP_PROFILE_H
+#define __KSU_UAPI_APP_PROFILE_H
 
-// Forward declarations
-struct cred;
+// we are on unity, we'll handle it there
+// #include <linux/types.h>
 
 #define KSU_APP_PROFILE_VER 2
 #define KSU_MAX_PACKAGE_NAME 256
-// NGROUPS_MAX for Linux is 65535 generally, but we only supports 32 groups.
+/* NGROUPS_MAX for Linux is 65535 generally, but we only supports 32 groups. */
 #define KSU_MAX_GROUPS 32
 #define KSU_SELINUX_DOMAIN 64
 
 struct root_profile {
-	int32_t uid;
-	int32_t gid;
+	__s32 uid;
+	__s32 gid;
 
-	int32_t groups_count;
-	int32_t groups[KSU_MAX_GROUPS];
+	__s32 groups_count;
+	__s32 groups[KSU_MAX_GROUPS];
 
-	// kernel_cap_t is u32[2] for capabilities v3
+	/* kernel_cap_t is u32[2] for capabilities v3 */
 	struct {
-		u64 effective;
-		u64 permitted;
-		u64 inheritable;
+		__u64 effective;
+		__u64 permitted;
+		__u64 inheritable;
 	} capabilities;
 
 	char selinux_domain[KSU_SELINUX_DOMAIN];
 
-	int32_t namespaces;
+	__s32 namespaces;
 };
 
 struct non_root_profile {
@@ -34,12 +34,15 @@ struct non_root_profile {
 };
 
 struct app_profile {
-	// It may be utilized for backward compatibility, although we have never explicitly made any promises regarding this.
-	u32 version;
+	/*
+	 * It may be utilized for backward compatibility, although we have never
+	 * explicitly made any promises regarding this.
+	 */
+	__u32 version;
 
-	// this is usually the package of the app, but can be other value for special apps
+	/* this is usually the package of the app, but can be other value for special apps */
 	char key[KSU_MAX_PACKAGE_NAME];
-	int32_t current_uid;
+	__s32 current_uid;
 	bool allow_su;
 
 	union {
@@ -57,10 +60,5 @@ struct app_profile {
 		} nrp_config;
 	};
 };
-
-// Escalate current process to root with the appropriate profile
-int escape_with_root_profile(void);
-
-void escape_to_root_forced(void);
 
 #endif
