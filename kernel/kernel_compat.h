2f96cc5b88a7712b5ff3281e68e725462155139f
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-09-10 16:28:06 +0800

    1

--- kernel/kernel_compat.h
+++ kernel/kernel_compat.h
@@ -313,12 +313,12 @@ static inline void ksu_static_key_disable(struct static_key *key)
 #define static_branch_unlikely(k)	static_key_false(k)
 #define static_branch_likely(k)		static_key_true(k)
 
-#ifdef CC_HAVE_ASM_GOTO
-#define DEFINE_STATIC_KEY_TRUE(k)	struct static_key k = { .enabled = ATOMIC_INIT(1), .entries = (void *)1 }
-#define DEFINE_STATIC_KEY_FALSE(k)	struct static_key k = { .enabled = ATOMIC_INIT(0), .entries = (void *)0 }
-#else
-#define DEFINE_STATIC_KEY_TRUE(k)	struct static_key k = { .enabled = ATOMIC_INIT(1) }
-#define DEFINE_STATIC_KEY_FALSE(k)	struct static_key k = { .enabled = ATOMIC_INIT(0) }
+#ifndef DEFINE_STATIC_KEY_FALSE
+#define DEFINE_STATIC_KEY_FALSE(k)	struct static_key k = STATIC_KEY_INIT_FALSE
+#endif
+
+#ifndef DEFINE_STATIC_KEY_TRUE
+#define DEFINE_STATIC_KEY_TRUE(k)	struct static_key k = STATIC_KEY_INIT_TRUE
 #endif
 
 #endif // < 4.3
