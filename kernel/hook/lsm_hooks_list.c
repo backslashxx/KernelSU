e4cc60620885dbe5f6e60126b0c13c17466aa0ed
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-09-15 13:38:04 +0800

    overload setprocattr

--- kernel/hook/lsm_hooks_list.c
+++ kernel/hook/lsm_hooks_list.c
@@ -72,22 +72,37 @@ capability_fn:
 }
 #endif
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0) || defined(KSU_COMPAT_SECURITY_ADD_HOOKS_V2)
-static int (*setprocattr_fn)(const char *name, void *value, size_t size) __read_mostly = nullptr;
-static __nocfi int ksu_setprocattr(const char *name, void *value, size_t size)
-{
-	ksu_hide_setprocattr_inline(name, value, size);
-	return setprocattr_fn(name, value, size);
+static void *setprocattr_fn __read_mostly = nullptr;
+#define SETPROCATTR_TYPE_old	struct task_struct *, char *, void *, size_t
+#define SETPROCATTR_TYPE_new1	const char *, void *, size_t
+#define SETPROCATTR_TYPE_new2	const char *lsm, const char *, void *, size_t
 
-}
-#else
-static int (*setprocattr_fn)(struct task_struct *p, char *name, void *value, size_t size) __read_mostly = nullptr;
-static __nocfi int ksu_setprocattr(struct task_struct *p, char *name, void *value, size_t size)
+static __nocfi int ksu_setprocattr_new(const char *name, void *value, size_t size)
 {
+	typeof(ksu_setprocattr_new) *_setprocattr_fn = setprocattr_fn;
 	ksu_hide_setprocattr_inline(name, value, size);
-	return setprocattr_fn(p, name, value, size);
+	return _setprocattr_fn(name, value, size);
 }
-#endif
+
+static __nocfi int ksu_setprocattr_old(struct task_struct *p, char *name, void *value, size_t size)
+{
+	typeof(ksu_setprocattr_old) *_setprocattr_fn = setprocattr_fn;
+	ksu_hide_setprocattr_inline(name, value, size);
+	return _setprocattr_fn(p, name, value, size);
+}
+
+#define OVERLOAD_SETPROCATTR(fn_p) _Generic((fn_p),			\
+	int (*)(SETPROCATTR_TYPE_old):	(void *)ksu_setprocattr_old,	\
+	int (*)(SETPROCATTR_TYPE_new1):	(void *)ksu_setprocattr_new, 	\
+	int (*)(SETPROCATTR_TYPE_new2):	(void *)ksu_setprocattr_new 	\
+)
+
+// now choose what we have
+static typeof(security_setprocattr) *ksu_setprocattr __read_mostly = OVERLOAD_SETPROCATTR(security_setprocattr);
+#undef SETPROCATTR_TYPE_new2
+#undef SETPROCATTR_TYPE_new1
+#undef SETPROCATTR_TYPE_old
+#undef OVERLOAD_SETPROCATTR
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0) || defined(KSU_COMPAT_SECURITY_DELETE_HOOKS_HLIST)
 static void ksu_hack_lsm_slot(struct hlist_head *hook_head, uintptr_t *old_ptr, uintptr_t new_ptr)
