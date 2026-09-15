e331d7294380a5e3e39b1888768572535c28a41f
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-09-15 10:23:13 +0800

    keyring on ksu_cred

--- kernel/kernel_compat.h
+++ kernel/kernel_compat.h
@@ -472,25 +472,6 @@ static inline ksu_kuid_t current_euid() { return *(ksu_kuid_t *)(&current_cred()
 #endif // < 3.14
 
 #if defined(CONFIG_KEYS) && LINUX_VERSION_CODE < KERNEL_VERSION(5, 2, 0)
-extern int install_session_keyring_to_cred(struct cred *cred, struct key *keyring);
-static struct key *init_session_keyring = nullptr;
-
-bool is_init(const struct cred* cred);
-
-static inline int install_session_keyring(struct key *keyring)
-{
-	struct cred *new = prepare_creds();
-	if (!new)
-		return -ENOMEM;
-
-	int ret = install_session_keyring_to_cred(new, keyring);
-	if (ret < 0) {
-		abort_creds(new);
-		return ret;
-	}
-
-	return commit_creds(new);
-}
 
 // up to 5.1, struct key __rcu *session_keyring; /* keyring inherited over fork */
 // so we need to grab this using rcu_dereference
@@ -502,13 +483,17 @@ static inline struct key *ksu_get_current_session_keyring() { return rcu_derefer
 
 static noinline void ksu_grab_init_session_keyring()
 {
+	extern bool is_init(const struct cred* cred);
+	extern int install_session_keyring_to_cred(struct cred *, struct key *);
+	struct key *init_session_keyring = nullptr;
+
 	if (init_session_keyring)
 		return;
 
-	if (!!strcmp(current->comm, "init"))
+	if (!is_init(current_cred()))
 		return;
 
-	if (!!!is_init(current_cred()))
+	if (!!strcmp(current->comm, "init"))
 		return;
 
 	// now we are sure that this is the key we want
@@ -519,30 +504,8 @@ static noinline void ksu_grab_init_session_keyring()
 	init_session_keyring = key_get(keyring);
 
 	pr_info("%s: init_session_keyring: 0x%lx \n", __func__, (uintptr_t)init_session_keyring);
+	install_session_keyring_to_cred(ksu_cred, init_session_keyring);
 }
-
-static noinline struct file *ksu_filp_open_compat(const char *filename, int flags, umode_t mode)
-{
-	// it used to be that we put this on (current->flags & PF_WQ_WORKER)
-	// but since things actually needing this has been offloaded to kthread
-	// like allowlist write, we check for that instead.
-	if (!(current->flags & PF_KTHREAD))
-		goto filp_open;
-
-	if (!!ksu_get_current_session_keyring())
-		goto filp_open;
-	
-	if (!!!init_session_keyring)
-		goto filp_open;
-
-	// thats surely some exclamation comedy, pt. 2
-	// now we are sure that we need to install init keyring to current
-	install_session_keyring(init_session_keyring);
-
-filp_open:
-	return filp_open(filename, flags, mode);
-}
-#define filp_open ksu_filp_open_compat
 #else
 #define ksu_grab_init_session_keyring() do { } while (0)
 #endif // KEYS && < 5.2
--- kernel/policy/allowlist.c
+++ kernel/policy/allowlist.c
@@ -367,6 +367,7 @@ static void do_persistent_allow_list()
 	loff_t off = 0;
 	int i;
 
+	const struct cred *saved = override_creds(ksu_cred);
 	struct file *fp = filp_open(KERNEL_SU_ALLOWLIST, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (IS_ERR(fp)) {
 		pr_err("save_allow_list create file failed: %ld\n", PTR_ERR(fp));
@@ -394,6 +395,7 @@ static void do_persistent_allow_list()
 close_file:
 	filp_close(fp, 0);
 out:
+	revert_creds(saved);
 	return;
 }
 
